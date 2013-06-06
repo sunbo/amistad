4978cefe5b45b72dc383c9307c0713d338402145
sunbo <yueyuegigi21@gmail.com>
2013-06-06 15:49:52 +0800

    Update active_record_friend_model.rb

--- lib/amistad/active_record_friend_model.rb
+++ lib/amistad/active_record_friend_model.rb
@@ -69,7 +69,8 @@ module Amistad
       return false if friendship.nil? || amistad_invited?(user)
       friendship.update_attribute(:pending, false)
     end
-
+    alias_method :amistad_invited?, :invited?
+    
     # deletes a friendship
     def remove_friendship(user)
       friendship = find_any_friendship_with(user)
@@ -153,7 +154,7 @@ module Amistad
     end
 
     # checks if a current user invited given user
-    def amistad_invited?(user)
+    def invited?(user)
       friendship = find_any_friendship_with(user)
       return false if friendship.nil?
       friendship.friend_id == user.id
